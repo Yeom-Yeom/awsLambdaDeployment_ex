25f7c3e6b046026f5ca55eb23220453e8b8cb375
Yeom-Yeom <diting9813@skuniv.ac.kr>
2024-11-21 15:45:19 +0900

    Updated Lambda function messge

--- app.py
+++ app.py
@@ -1,17 +1,7 @@
 import json
 
 def lambda_halnder(event, context):
-    print("Event:", event)
-
-    response = {
+    return {
         "statusCode": 200,
-        "headers": {
-            "Content-Type": "application/json"
-        },
-        "body": json.dumps({
-            "message": "Hello from AWS Lambda!",
-            "input": event
-        })
+        "body": "Hello, AWS Lambda!"
     }
-
-    return response
